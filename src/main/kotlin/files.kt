5eff668922d2a054502d46a7f40982e8414aa7da
Vadim <fullmetallboss@gmail.com>
2023-11-17 18:39:55 +0300

    Stage-1

---
+++ src/main/kotlin/files.kt
@@ -0,0 +1,12 @@
+import java.io.File
+
+fun main() {
+
+    val wordsFile = File("words.txt")
+    wordsFile.createNewFile()
+    wordsFile.writeText("hello привет\n")
+    wordsFile.appendText("dog собака\n")
+    wordsFile.appendText("cat кошка\n")
+
+    wordsFile.readLines().forEach { println(it) }
+}
