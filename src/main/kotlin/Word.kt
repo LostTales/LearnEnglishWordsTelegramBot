68dba56fa6a25b2cd8012874dd670a930222d376
Vadim <fullmetallboss@gmail.com>
2023-11-18 17:47:37 +0300

    Stage-2 v1.1

--- src/main/kotlin/Word.kt
+++ src/main/kotlin/Word.kt
@@ -1,5 +1,5 @@
 data class Word(
     val original: String,
     val translate: String,
-    var correctAnswersCount: String? = "0",
+    var correctAnswersCount: Int = 0,
 )
--- src/main/kotlin/files.kt
+++ src/main/kotlin/files.kt
@@ -3,19 +3,21 @@ import java.io.File
 fun main() {
 
     val wordsFile = File("words.txt")
-    wordsFile.createNewFile()
-    wordsFile.writeText("hello|привет|25\n")
-    wordsFile.appendText("dog|собака|10\n")
-    wordsFile.appendText("cat|кошка|5\n")
 
     val dictionary = mutableListOf<Word>()
 
     wordsFile.readLines().forEach {
         val line = it.split("|")
-        val answersCount = line[2]
-        val word = Word(original = line[0], translate = line[1], correctAnswersCount = answersCount ?: "0")
+        val correctAnswers = line[2]?.toInt() ?: 0
+        val word = Word(original = line[0], translate = line[1], correctAnswersCount = correctAnswers)
         dictionary.add(word)
     }
 
     dictionary.forEach { println(it) }
+}
+
+fun File.createDictionary(text: String){
+
+    createNewFile()
+    appendText(text)
 }
